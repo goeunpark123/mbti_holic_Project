5c9004405b0746191ed6fd8b49c735b47fdd3a13
Goeun Park <goeunpark21@gmail.com>
2020-06-09 22:36:27 +0900

    Firebase 콘솔에서 푸시 알림 보내기

--- app/src/main/java/com/example/mobileprogramming_mbtiholic/Main/MainActivity.java
+++ app/src/main/java/com/example/mobileprogramming_mbtiholic/Main/MainActivity.java
@@ -2,11 +2,12 @@ package com.example.mobileprogramming_mbtiholic.Main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.mobileprogramming_mbtiholic.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) { //여기에서 시작해서 로그인 했으면 MainTabActivity로 넘어가게 분기를 해줘야된다.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            String token = FirebaseInstanceId.getInstance().getToken();
+            Log.d("IDService","device token : "+token);
+        }
+
+        catch (NullPointerException e) {
+            e.printStackTrace();
+        }
 
         LoginCheckAsyncTask asyncTask = new LoginCheckAsyncTask(FirebaseAuth.getInstance(), this);
         asyncTask.execute();
